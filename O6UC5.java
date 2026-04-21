09dd351ae014498a9ca0f127a61de3140ae45eea
sugandhan-baskaran <sugandhanbaskaran@gmail.com>
2026-04-21 09:34:46 +0530

    Added UC5 validate user move

---
+++ O6UC5.java
@@ -0,0 +1,18 @@
+public class O6UC5 {
+
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isValidMove(1, 1));
+    }
+
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+               col >= 0 && col < 3 &&
+               board[row][col] == '-';
+    }
+}
